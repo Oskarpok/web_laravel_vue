4593b7ba09c03ffae398b7460ed7bdd691a03853
OskarP <oskar43619@wp.pl>
2025-07-31 00:20:12 +0200

    backend cms params enums and model upgrate to ne structure

--- backend/packages/cms/src/Enums/ParamsType.php
+++ backend/packages/cms/src/Enums/ParamsType.php
@@ -11,20 +11,9 @@ enum ParamsType: int {
   case Boolean = 4;
   case Json = 5;
   
-  public function label(): string {
-    return match($this) {
-      self::Unverified => 'Unverified',
-      self::Integer => 'Integer',
-      self::Float => 'Float',
-      self::String => 'String',
-      self::Boolean => 'Boolean',
-      self::Json => 'Json',
-    };
-  }
-
   public static function toArray(): array {
     return collect(self::cases())
-      ->mapWithKeys(fn($case) => [$case->value => $case->label()])->toArray();
+      ->mapWithKeys(fn($case) => [$case->value => $case->name])->toArray();
   }
 
 }
---
+++ backend/packages/cms/src/Models/Cms/Param.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace User\LaravelCms\Models\Cms;
+
+use User\LaravelCms\Models\Base;
+
+class Param extends Base {
+  
+  protected $fillable = [
+    'name', 'type', 'val_string', 'val_int', 'val_float', 'val_bool', 'val_json'
+  ];
+ 
+  public static function validationRules(): array {
+    return [
+      'name' => ['required', 'string', 'max:255'],
+      'type' => ['required', 'integer'],
+      'val_string' => ['nullable'],
+      'val_int' => ['nullable'],
+      'val_float' => ['nullable'],
+      'val_bool' => ['nullable'],
+      'val_json' => ['nullable'],
+    ];
+  }
+
+}
