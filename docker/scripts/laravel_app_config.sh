d9a43ae0b1fc65af3b2cbd575aecbbc3341226e6
OskarP <oskar43619@wp.pl>
2025-07-30 12:34:52 +0200

    Update laravel_app_config.sh

--- docker/scripts/laravel_app_config.sh
+++ docker/scripts/laravel_app_config.sh
@@ -3,6 +3,11 @@
 wait-for-it.sh mysql:3306 --timeout=60 --strict -- 
 
 cd /var/www/html/${PROJECT}
+
+if [ ! -d "vendor" ]; then
+  composer install --no-interaction --prefer-dist --optimize-autoloader
+fi
+
 php artisan migrate --force --no-interaction
 
 exec php-fpm
