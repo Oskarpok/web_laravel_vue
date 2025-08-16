529986ee1e5020eed72d7238f7d6c45711fc2d63
OskarP <oskar43619@wp.pl>
2025-08-16 21:57:39 +0200

    Update dashboard.blade.php

--- backend/packages/cms/resources/views/dashboard.blade.php
+++ backend/packages/cms/resources/views/dashboard.blade.php
@@ -6,8 +6,6 @@
 			href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" 
 			crossorigin="anonymous" 
 			referrerpolicy="no-referrer"/>
-		<script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
-		<script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js"></script>
     @vite(['resources/css/cms.css', 'resources/js/cms.js'])
 	</head>
 	<body class="cms-primary-color text-white">
