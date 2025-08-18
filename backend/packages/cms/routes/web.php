3d64de7821c7432c73eda128d697236f2a5fae88
OskarP <oskar43619@wp.pl>
2025-08-18 10:40:39 +0200

    Update web.php

--- backend/packages/cms/routes/web.php
+++ backend/packages/cms/routes/web.php
@@ -5,5 +5,7 @@ use Illuminate\Support\Facades\Route;
 use User\LaravelCms\Http\Controllers\Cms\ParamController;
 use User\LaravelCms\Http\Controllers\Cms\UserController;
 
-Route::resource('params', ParamController::class);
-Route::resource('users', UserController::class);
+Route::prefix('cms')->group(function () {
+  Route::resource('params', ParamController::class);
+  Route::resource('users', UserController::class);
+});
